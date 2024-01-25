70b1d3ebd04752ea495dbc19f4e185823cb46cb7
NenadoPL1Z <limon4ikpro1@gmail.com>
2024-01-25 10:25:53 +0500

    first commit

---
+++ src/app/blog/[id]/page.tsx
@@ -0,0 +1,46 @@
+import { Metadata } from "next";
+
+async function getData(id: string) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`,
+    {
+      next: {
+        revalidate: 60,
+      },
+    },
+  );
+
+  if (!response.ok) {
+    throw new Error("qwe");
+  }
+
+  return response.json();
+}
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export async function generateMetadata({
+  params: { id },
+}: Props): Promise<Metadata> {
+  const post = await getData(id);
+
+  return {
+    title: post.title,
+  };
+}
+
+export default async function Post({ params: { id } }: Props) {
+  const post = await getData(id);
+  return (
+    <>
+      <h1>
+        {post.title} {id}
+      </h1>
+      <p>{post.body}</p>
+    </>
+  );
+}
